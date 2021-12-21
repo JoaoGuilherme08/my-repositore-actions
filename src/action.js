342d5f2ad0d39a60fea40dbd8cec493e889edada
João Guilherme Cobo <joaocobo20@gmail.com>
2021-12-21 17:30:41 -0300

    aa

--- src/action.js
+++ src/action.js
@@ -24,7 +24,7 @@ async function run() {
 
   const randomPos = Math.round(Math.random() * 1000);
   const url =
-    "https://api.tenor.com/v1/search?q=thank%20you&pos=$%22$13%22&mit=1li&media_filter=minimal&contentfilter=high&key=%22$HS0UNSXONQCL";
+    "https://api.tenor.com/v1/search?q=love&pos=$%22$13%22&mit=1li&media_filter=minimal&contentfilter=high&key=%22$HS0UNSXONQCL";
     
   console.log(`Searching Tenor: ${url}`);
 
