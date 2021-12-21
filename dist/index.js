0ccc2e95863fabf6c43be92374e095d8c11c8b5a
João Guilherme Cobo <joaocobo20@gmail.com>
2021-12-21 16:00:20 -0300

    Agora vai certeza

--- dist/index.js
+++ dist/index.js
@@ -8447,7 +8447,8 @@ const github = __nccwpck_require__(5016);
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    
+    const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
+
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
--- src/action.js
+++ src/action.js
@@ -3,7 +3,8 @@ const github = require('@actions/github');
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    
+    const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
+
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
